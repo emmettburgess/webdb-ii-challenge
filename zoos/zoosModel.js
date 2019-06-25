810f232b49316fd6fb5568827daa363195c4f219
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-06-25 13:55:01 -0600

    functions

---
+++ zoos/zoosModel.js
@@ -0,0 +1,56 @@
+const knex = require('knex');
+
+const knexConfig = {
+    clinet: 'sqlite3',
+    connection: {
+        filename: "./data/lambda.db3"
+    },
+    useNullAsDefault: true,
+    debug: true
+};
+
+const db = knex(knexConfig);
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+};
+
+function find() {
+    return db('zoos')
+};
+
+function findById() {
+    return db('zoos')
+        .where ({ id })
+        .first()
+};
+
+function add(zoo) {
+    return db('zoos')
+        .insert(zoo, 'id')
+        .then(ids => {
+            const [id] = ids;
+            return findById(id)
+        })
+};
+
+function update(id, changes) {
+    return db('zoos')
+        .where ({ id })
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                return findById(id);
+            } else {
+                return null;
+            }
+        })
+};
+
+function remove() {
+    return null
+};
