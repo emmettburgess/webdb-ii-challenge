c4cefa940d923440ffcfa744512c3939c1622c2f
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-06-25 14:07:26 -0600

    zoo router holy crap

---
+++ zoos/zoosRouter.js
@@ -0,0 +1,79 @@
+const knex = require('knex');
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: "./data/lambda.db3"
+    },
+    useNullAsDefault: true,
+    debug: true
+};
+const db = knex(knexConfig);
+const router = require('express').Router();
+const Zoos = require('./zoosModel');
+
+router.get("/", (req, res) => {
+    Zoos.find()
+        .then(zoos => {
+            res.status(200).json(zoos);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.get("./id", (req, res) => {
+    Zoos.findById(req.params.id)
+        .then(zoo => {
+            if (zoo) {
+                res.status(200).json(zoo);
+            } else {
+                res.status(404).json({message: 'Record not found'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        });
+});
+
+router.post("/", (res, req) => {
+    Zoos.add(req.body, "id")
+        .then(zoo => {
+            res.status(201).json(zoo)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        });
+});
+
+router.put("/.id", (req, res) => {
+    Zoos.update(req.params.id, req.body)
+        .then(count => {
+            if (count) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({message: `Zoo ${count} not found`})
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+});
+
+router.delete("./id", (req, res) => {
+    db('zoos')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if (count > 0) {
+                const message = count > 1 ? `records` : 'record'
+                res.status(200).json({message: `${count} ${unit} deleted`})
+            } else {
+                res.status(404).json({message: 'Zoo not found'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        });
+});
+
+module.exports = router;
